d4d16639aa9f1a48a70c5424985efd45e912a8cd
Ernest V <ernest.viol@gmail.com>
2026-01-12 14:00:21 -0800

    Add methods to notes and projects
    Added some predicted methods to the project and note components.

--- src/components/note.js
+++ src/components/note.js
@@ -1,5 +1,43 @@
 // make a factory function or a class constructor
+// factory functions are smaller objects that don't get changed often.
+// title, description, dueDate, priority
+export default class Note {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority; // I'd rather project maintain priority using some sort of list
+  }
 
-export default function note() {
+  get title() {
+    return this.title;
+  }
 
+  set title(value) {
+    this.title = value;
+  }
+
+  get description() {
+    return this.description;
+  }
+
+  set description(value) {
+    this.description = value;
+  }
+
+  get dueDate() {
+    return this.dueDate;
+  }
+
+  set dueDate(value) {
+    this.dueDate = value;
+  }
+
+  get priority() {
+    return this.priority;
+  }
+
+  set priority(value) {
+    this.priority = value;
+  }
 }
--- src/components/project.js
+++ src/components/project.js
@@ -1,4 +1,27 @@
 // make a factory function or a class constructor
-export default function project() {
+export default class Project {
+  constructor(name) {
+    this.notes = [];
+    this.name = name;
+  }
 
+  get name() {
+    return this.name;
+  }
+
+  set name(value) {
+    this.name = value;
+  }
+
+  add(note) {
+    this.notes.append(note);
+  }
+
+  remove(note) {
+    //find the array location of the note then delete it
+  }
+
+
+  // add
+  // delete
 }
